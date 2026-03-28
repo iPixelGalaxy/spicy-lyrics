c16619afbf0edd60489b4121c51054f2c8eada31
iPixelGalaxy <github@ipixelgalaxy.com>
2026-03-27 18:52:39 -0700

    Create UpdateDialog.tsx

---
+++ src/components/ReactComponents/UpdateDialog.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface UpdateDialogProps {
+  fromVersion: string;
+  spicyLyricsVersion: string;
+}
+
+const UpdateDialog: React.FC<UpdateDialogProps> = ({ fromVersion, spicyLyricsVersion }) => {
+  return (
+    <div className="update-card-wrapper slm">
+      <h2 className="header">Spicy Lyrics has been successfully updated!</h2>
+      <div className="card version">
+        Version: {fromVersion ? `${fromVersion} → ` : ""}{spicyLyricsVersion || "Unknown"}
+      </div>
+      <button
+        className="card btn btn-release"
+        onClick={() =>
+          window.open(
+            `https://github.com/Spikerko/spicy-lyrics/releases/tag/${spicyLyricsVersion}`,
+            "_blank"
+          )
+        }
+      >
+        Release Notes →
+      </button>
+      <button
+        className="card btn btn-discord"
+        onClick={() => window.open("https://discord.com/invite/uqgXU5wh8j", "_blank")}
+      >
+        <p>Join our Discord Server! →</p>
+      </button>
+    </div>
+  );
+};
+
+export default UpdateDialog;

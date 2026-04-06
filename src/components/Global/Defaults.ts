bed89aa2e4132c1fb782b6882c90773f591c5342
iPixelGalaxy <github@ipixelgalaxy.com>
2026-04-05 23:20:57 -0700

    feat: Enable Prioritize Apple Music Quality by default

--- src/components/Global/Defaults.ts
+++ src/components/Global/Defaults.ts
@@ -49,7 +49,7 @@ const Defaults = {
   LyricsSourceOrder: [...DEFAULT_LYRICS_SOURCE_ORDER],
   DisabledLyricsSourceIds: ["lrclib", "netease"] as LyricsSourceProviderId[],
   IgnoreMusixmatchWordSync: true,
-  PrioritizeAppleMusicQuality: false,
+  PrioritizeAppleMusicQuality: true,
 };
 
 export default Defaults;
